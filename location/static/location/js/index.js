f8a80b54c68f1de70acb7965b471407ea7500542
wangguirong <wangguirong@ad61v1.com>
2020-03-10 17:43:40 +0800

    优化前端界面

--- location/static/location/js/index.js
+++ location/static/location/js/index.js
@@ -7,33 +7,32 @@ $(function () {
         };
         $.get("search_location", params, function (data) {
             var full_address = $("#full_address");
-            var res = data;
             var location1 = $("#location1"), location2 = $("#location2");
             full_address.empty();
-            if (res.code === 0){
-                for (i in res.full_address_list) {
-                var address = $("<li></li>").text(res.full_address_list[i]);
+            if (data.code === 0){
+                for (i in data.full_address_list) {
+                var address = $("<li></li>").text(data.full_address_list[i]);
                 full_address.append(address);
                 }
                 if (location1.val() === "") {
-                    location1.val(res.location);
+                    location1.val(data.location);
                 }
                 else if (location2.val() === "") {
-                    location2.val(res.location);
+                    location2.val(data.location);
                 }
                 else {
-                    location1.val(res.location);
+                    location1.val(data.location);
                     location2.val("");
                 }
             }
-            else if (res.code === 1){
-                full_address.append(res.error);
+            else if (data.code === 1){
+                full_address.append(data.error);
             }
         });
     });
 
     $("#search_transit").click(function () {
-        var order_name = $("#order_name");
+        let order_name = $("#order_name");
         var data = {
             location1: $("#location1").val(),
             location2: $("#location2").val(),
@@ -42,21 +41,24 @@ $(function () {
             order_type: $("#order_type").val()
         };
         $.post("/search_transit_direction", data, function (data) {
-            console.log(typeof(data));
-            let res = data;
             let table = $("#transit_table");
-            table.empty();
-            if (res.code === 0) {
+            let tableHeader = $("#table_header");
+            let tableBody = $("#table_body");
+            tableHeader.empty();
+            tableBody.empty();
+            console.log(data.code);
+            if (data.code === 0) {
                 let header = "<tr><th>id</th><th>站点名</th><th>" + order_name.find("option:selected").text() + "</th></tr>";
-                table.append(header);
-                for (t in res.transit_detail) {
-                    let detail = "<tr><td>" + res.transit_detail[t].traffic_info_id + "</td><td><a href='detail/" + res.transit_detail[t].traffic_info_id + "'>" + res.transit_detail[t].traffic_info__name + "</a></td><td>" + res.transit_detail[t][$("#order_name").val()] + "</td></tr>";
-                    console.log(res.transit_detail[t].traffic_info__name);
-                    table.append(detail);
+                tableHeader.append(header);
+                console.log(header);
+                for (t in data.transit_detail) {
+                    let detail = "<tr><td>" + data.transit_detail[t].traffic_info_id + "</td><td><a href='detail/" + data.transit_detail[t].traffic_info_id + "'>" + data.transit_detail[t].traffic_info__name + "</a></td><td>" + data.transit_detail[t][$("#order_name").val()] + "</td></tr>";
+                    console.log(data.transit_detail[t].traffic_info__name);
+                    tableBody.append(detail);
                 }
             }
-            else if (res.code === 1) {
-                table.append(res.error);
+            else if (data.code === 1) {
+                table.append(data.error);
             }
         });
     });
